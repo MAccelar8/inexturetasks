92b3ec69e437cdf7b966dc237f31069a03938c6b
Milan Lakhani <lakhanimilan9@gmail.com>
2020-01-03 20:23:42 +0530

    Estrada : Hero-page, about ,Misc , work sections

---
+++ Ave Estrada/js/custom.js
@@ -0,0 +1,33 @@
+var containerEl = document.querySelector(".work-filter");
+var mixer;
+
+if (containerEl) {
+  mixer = mixitup(containerEl, {
+    selectors: {
+      control: "[data-filter]"
+    },
+    load: {
+      filter: ".mix"
+    }
+  });
+}
+
+
+$(".grid").masonry({
+  // options
+  itemSelector: ".grid-item",
+  columnWidth: ".grid-sizer",
+  percentPosition: true,
+  gutter: ".gutter-sizer"
+});
+
+$('.work-filter').on('mixEnd', function() {
+    
+$(".grid").masonry({
+    // options
+    itemSelector: ".grid-item",
+    columnWidth: ".grid-sizer",
+    percentPosition: true,
+    gutter: ".gutter-sizer"
+  });
+  });
